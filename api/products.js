e255f93d04bd78831ddd5ece7ee187fbdbc72290
sarveshprajapati923-stack <sarveshprajapati923@gmail.com>
2026-09-13 14:02:31 +0530

    Update products.js

--- api/products.js
+++ api/products.js
@@ -4,31 +4,31 @@ const products = [
     name: 'Wheel Active 2 in 1 Detergent Powder - 1kg',
     price: 75,
     category: 'Household',
-    image: '/images/wheel.jpg'
+    image: '/images/IMG-20260913-WA0001.jpg'
   },
   {
     id: 2,
     name: 'Ghadi Detergent Powder - 3kg',
     price: 210,
     category: 'Household',
-    image: '/images/ghadi.jpg'
+    image: '/images/IMG-20260913-WA0002.jpg'
   },
   {
     id: 3,
     name: 'Tide Naturals Detergent Powder - 3kg',
     price: 235,
     category: 'Household',
-    image: '/images/tide.jpg'
+    image: '/images/IMG-20260913-WA0003.jpg'
   },
   {
     id: 4,
     name: 'Closeup Toothpaste - 300g Pack of 2',
     price: 160,
     category: 'Personal Care',
-    image: '/images/closeup.jpg'
+    image: '/images/IMG-20260913-WA0004.jpg'
   }
 ];
 
 export default function handler(req, res) {
   res.status(200).json(products);
-}
+             }
